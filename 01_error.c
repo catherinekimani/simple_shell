0a287d2e8e36613f33be2d875ab0805cb0cf75f5
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2023-05-25 00:36:51 +0000

    update

---
+++ 01_error.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+char *err_126(char **args);
+char *err_127(char **args);
+
+/**
+ *err_126 - Permission denied
+ *@args: An array of args
+ *
+ *Return: The error
+ */
+char *err_126(char **args)
+{
+	int length;
+	char *err, *history_str;
+
+	history_str = _itoa(history);
+	if (!history_str)
+		return (NULL);
+	length = custom_strlen(name) + custom_strlen(history_str) +
+		custom_strlen(args[0]) + 24;
+	err = malloc(sizeof(char) * (length + 1));
+	if (!err)
+	{
+		free(history_str);
+		return (NULL);
+	}
+	custom_strcpy(err, name);
+	custom_strcat(err, ": ");
+	custom_strcat(err, history_str);
+	custom_strcat(err, ": ");
+	custom_strcat(err, args[0]);
+	custom_strcat(err, "Permission denied\n");
+	free(history_str);
+	return (err);
+}
+/**
+ *err_127 - Create error msg for cmd not found
+ *@args: An arr args
+ *
+ *Return: The error
+ */
+char *err_127(char **args)
+{
+	char *err, *history_str;
+	int length;
+
+	history_str = _itoa(history);
+	if (!history_str)
+		return (NULL);
+	length = custom_strlen(name) + custom_strlen(history_str) +
+		custom_strlen(args[0]) + 16;
+	err = malloc(sizeof(char) * (length + 1));
+	if (!err)
+	{
+		free(history_str);
+		return (NULL);
+	}
+	custom_strcpy(err, name);
+	custom_strcat(err, ": ");
+	custom_strcat(err, history_str);
+	custom_strcat(err, ": ");
+	custom_strcat(err, args[0]);
+	custom_strcat(err, ": cmd not found\n");
+	free(history_str);
+	return (err);
+}
